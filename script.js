542446345cd9922482d499684ce370141361368c
carl.dunne <developerdunne@gmail.com>
2023-10-26 14:15:28 +0100

    Added event listeners to button rolls

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 //Selecting Elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.querySelector("#current--0");
+const current1El = document.querySelector("#current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -12,12 +14,22 @@ score0El.textContent = 0;
 score1El.textContent - 0;
 diceEl.classList.add("hidden");
 
+let currentScore = 0;
+
 //Rolling dice function
 
 btnRoll.addEventListener("click", function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
   diceEl.classList.remove("hidden");
   diceEl.src = `assets/images/dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+  }
 });
 
 btnNew.addEventListener("click", function () {});
